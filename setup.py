a12bc24239ea3bb6323035b0a8763269c8bab657
Stanislav Komanec <stanislav@komanec.eu>
2014-07-27 12:51:31 +0200

    Update setup

--- setup.py
+++ setup.py
@@ -7,11 +7,8 @@ base_dir = os.path.dirname(os.path.abspath(__file__))
 
 setup(
     name = "mobilem-api",
-    version = "0.0.1b",
+    version = "0.0.2a",
     description = "A library for sending SMS via mobilem.cz gateway.",
-    long_description="\n\n".join([
-        open(os.path.join(base_dir, "README.md"), "r").read()
-    ]),
     url = "https://github.com/netman92/mobilem_cz",
     author = "Stanislav Komanec",
     author_email = "stanislav@komanec.eu",
